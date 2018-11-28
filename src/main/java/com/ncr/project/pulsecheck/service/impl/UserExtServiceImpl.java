9955ac0567c8f9ac5d8e2425ec6dccdba0c93e7a
Diego De Cao <dd185209@ncr.com>
2018-11-28 17:18:51 +0100

    fix delete user

--- src/main/java/com/ncr/project/pulsecheck/service/impl/UserExtServiceImpl.java
+++ src/main/java/com/ncr/project/pulsecheck/service/impl/UserExtServiceImpl.java
@@ -1,6 +1,12 @@
 package com.ncr.project.pulsecheck.service.impl;
 
+import com.ncr.project.pulsecheck.service.ClientLeadService;
+import com.ncr.project.pulsecheck.service.OrgAdminService;
+import com.ncr.project.pulsecheck.service.ParticipantService;
 import com.ncr.project.pulsecheck.service.UserExtService;
+import com.ncr.project.pulsecheck.domain.ClientLead;
+import com.ncr.project.pulsecheck.domain.OrgAdmin;
+import com.ncr.project.pulsecheck.domain.Participant;
 import com.ncr.project.pulsecheck.domain.UserExt;
 import com.ncr.project.pulsecheck.repository.UserExtRepository;
 import com.ncr.project.pulsecheck.service.dto.UserExtDTO;
@@ -40,16 +46,28 @@ public class UserExtServiceImpl implements UserExtService {
     private final UserEventsVMMapper userEventsVMMapper;
     
     private final OrganizationAndEventsVMMapper organizationAndEventsVMMapper;
+
+    private final ClientLeadService clientLeadService;
     
+    private final OrgAdminService orgAdminService;
+    
+    private final ParticipantService participantService;
 
     public UserExtServiceImpl(UserExtRepository userExtRepository
     , UserExtMapper userExtMapper
     , UserEventsVMMapper userEventsVMMapper
-    , OrganizationAndEventsVMMapper organizationAndEventsVMMapper) {
+    , OrganizationAndEventsVMMapper organizationAndEventsVMMapper
+    ,ClientLeadService clientLeadService
+    ,OrgAdminService orgAdminService
+    ,ParticipantService participantService
+    ) {
         this.userExtRepository = userExtRepository;
         this.userExtMapper = userExtMapper;
         this.userEventsVMMapper = userEventsVMMapper;
         this.organizationAndEventsVMMapper = organizationAndEventsVMMapper;
+        this.clientLeadService = clientLeadService;
+        this.orgAdminService=orgAdminService;
+        this.participantService = participantService;
     }
 
     /**
@@ -161,6 +179,21 @@ public class UserExtServiceImpl implements UserExtService {
     @Override
     public void delete(Long id) {
         log.debug("Request to delete UserExt : {}", id);
+        userExtRepository.findById(id).ifPresent(userExt -> {
+            OrgAdmin orgAdmin = userExt.getOrgAdmin();
+            if(orgAdmin != null){
+                orgAdminService.delete(orgAdmin.getId());
+            }
+            ClientLead clientLead = userExt.getClientLead();
+            if(clientLead != null){
+                clientLeadService.delete(clientLead.getId());
+            }
+            Participant participant = userExt.getParticipant();
+            if (participant != null){
+                participantService.delete(participant.getId());
+            }
+        });
+        
         userExtRepository.deleteById(id);
     }
 
